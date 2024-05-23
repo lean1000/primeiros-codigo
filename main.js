9228a8c46437eec6d5688ed4c5124ec5e4e782b7
lean1000 <166569100+lean1000@users.noreply.github.com>
2024-05-23 11:43:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+// colocar comentario aqui
+
+console.log("hello word")
+
+//tipos de vareaveis
+  var rua,bairro,cep,estado,cidade,nome,
+  email,telefone,idade,genero,nhouse,vpmd18,
+  vpcdm
+
+rua = "esmeralda" //String
+bairro = "jardin orquidea" //String
+cep = "1345-766" //String
+estado = "São Paulo" //String
+cidade = "Campinas" //String
+nome = "Leandro" //String
+email = "calabreso@gmail.com" //String
+telefone = "55+ 19 99678-4567" //String
+idade = 24 //int
+genero = "Masculino" //String
+nhouse = 234 //int
+vpmd18 = (true)
+vpcdm = (false)
+
+  console.log("")
+  console.log("Informaçoes Pessoais")
+  console.log("--------------------------------------------------------")
+  console.log("Nome:",nome,"idade:",idade,"Genero:",genero)
+  console.log("--------------------------------------------------------")
+  console.log("Telefone:",telefone)
+  console.log("--------------------------------------------------------")
+  console.log("Rua:",rua,"Bairro:",bairro,"Numero da Casa:",nhouse)
+  console.log("CEP:",cep,"Estado:",estado,"Cidade:",cidade)
+  console.log("--------------------------------------------------------")
+  console.log("Email:",email)
+  console.log("--------------------------------------------------------")
+  console.log("Voce possui mais de 18 anos:",vpmd18)
+  console.log("Voce possui abilitacao:",vpcdm)
---
+++ tabuada.js
@@ -0,0 +1,27 @@
+console.log("Hello Word")
+
+console.log("")
+
+
+  var numerodasoma = 17
+  var numeroescolido = 17
+
+//var numero e o valor que define a soma
+
+
+
+  console.log("tabuada do 7")
+  console.log("-------------")
+  console.log(numeroescolido,"x 1=",numerodasoma*1)
+  console.log(numeroescolido,"x 2=",numerodasoma*2)
+  console.log(numeroescolido,"x 3=",numerodasoma*3)
+  console.log(numeroescolido,"x 4=",numerodasoma*4)
+  console.log(numeroescolido,"x 5=",numerodasoma*5)
+  console.log(numeroescolido,"x 6=",numerodasoma*6)
+  console.log(numeroescolido,"x 7=",numerodasoma*7)
+  console.log(numeroescolido,"x 8=",numerodasoma*8)
+  console.log(numeroescolido,"x 9=",numerodasoma*9)
+  console.log(numeroescolido,"x 10=",numerodasoma*10)
+  
+  
+  
