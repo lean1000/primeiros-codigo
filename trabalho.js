6b58225c89df3eb0ca71d309d5de9687d5349904
lean1000 <166569100+lean1000@users.noreply.github.com>
2024-05-24 09:20:08 -0300

    Add files via upload

---
+++ trabalho.js
@@ -0,0 +1,51 @@
+//primeira tarefa contas simples -+/*
+  var numero1, numero2
+
+    numero1 = 8
+    numero2 = 2 
+
+ console.log("TAREFA 1")
+ console.log("========")
+ console.log("8+2=",numero1+numero2)
+ console.log("8x2=",numero1*numero2)
+ console.log("8/2=",numero1/numero2)
+ console.log("8-2=",numero1-numero2)
+ console.log("")
+ console.log("")
+
+// converter fahrenheit em celsius
+// converter celsius em fahrenheit
+  var fahrenheit,celsius
+
+    fahrenheit = 102   
+    celsius = 36.9
+
+    var comvercaoDofahrenheit = fahrenheit-32
+    var comvercaoDocelsius = celsius*1.8
+
+ console.log("TAREFA 2,3")
+ console.log("==========")
+ console.log("2- 102 graus fahrenheit em celsius =",comvercaoDofahrenheit/1.8)
+ console.log("3- 36.9 graus celsius em fahrenheit =",comvercaoDocelsius+32)
+ console.log("")
+ console.log("")
+
+ // converter metros em centimetros
+  var Metros = 100
+    var metrosEmcentimeto = Metros * 100
+
+ console.log("TAREFA 4")
+ console.log("========")
+ console.log("100 Metros em centimetros =",metrosEmcentimeto,"centimetros")
+ console.log("")
+ console.log("")
+
+ // dar um desconto em um valor de produto
+  var CamisaDeTime = 139.99
+   var desconto = CamisaDeTime%40
+
+ console.log("TAREFA 5")
+ console.log("========")
+ console.log("O valor do produto com desconto e de",CamisaDeTime-desconto)
+ console.log("")
+ console.log("")
